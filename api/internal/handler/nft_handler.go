8af45d81305e93f176b012b0a59785746fc87841
Ze Huang <ze@dime.trade>
2023-01-24 10:54:13 -0800

    Add handler template + wallet proof API

---
+++ api/internal/handler/nft_handler.go
@@ -0,0 +1,7 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func UpdateNftBackstoryHandler(c *gin.Context) {
+
+}
---
+++ api/internal/handler/story_handler.go
@@ -0,0 +1,11 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func GetStoryChaptersHandler(c *gin.Context) {
+
+}
+
+func GetStoryChapterContentsHandler(c *gin.Context) {
+
+}
