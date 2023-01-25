728ffc49bdbecffcc848ea19239c43700d0e5279
Ze Huang <ze@dime.trade>
2023-01-25 09:02:51 -0800

    Add more indexed to tables

--- api/migrations/000005_create_story_info_table.up.sql
+++ api/migrations/000005_create_story_info_table.up.sql
@@ -11,6 +11,9 @@ CREATE TABLE IF NOT EXISTS story_info (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
+CREATE INDEX idx_story_info_on_franchise_id ON story_info(franchise_id);
+CREATE INDEX idx_story_info_on_seq_num ON story_info(seq_num);
+
 CREATE TRIGGER set_updated_at
 BEFORE UPDATE ON story_info
 FOR EACH ROW
