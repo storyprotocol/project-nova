3dc76df6263c787fc46cd4e133b6e8c11842677c
Ze Huang <edisonz0718@gmail.com>
2023-02-20 22:33:11 -0800

    Add Story content s3 implementation

--- api/internal/repository/story_content.go
+++ api/internal/repository/story_content.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/project-nova/backend/api/internal/constant"
+	"github.com/project-nova/backend/pkg/s3"
 )
 
 type StoryContentRepository interface {
@@ -23,14 +27,38 @@ type StoryMediaModel struct {
 	Description string `json:"description,omitempty"`
 }
 
-func NewStoryContentS3Impl(s3Client string) StoryContentRepository {
-	return &storyContentS3Impl{
-		s3Client: s3Client,
+func NewStoryContentS3Impl(s3Client s3.S3Client) (StoryContentRepository, error) {
+	contentMap := make(map[string][]*StorySectionModel)
+
+	keys, err := s3Client.ListObjectsNonRecursive(constant.S3ProjectNovaBucket)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list objects for %s: %v", constant.S3ProjectNovaBucket, err)
 	}
+
+	for _, key := range keys {
+		buf := aws.NewWriteAtBuffer([]byte{})
+		_, err := s3Client.DownloadObject(buf, constant.S3ProjectNovaBucket, *key+"/"+constant.S3ContentObject)
+		if err != nil {
+			return nil, fmt.Errorf("failed to download content from s3 for object %s: %v", *key, err)
+		}
+
+		var data []*StorySectionModel
+		err = json.Unmarshal(buf.Bytes(), &data)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal story content file data, error: %v", err)
+		}
+		contentMap[*key] = data
+	}
+
+	return &storyContentS3Impl{
+		s3Client:   s3Client,
+		contentMap: contentMap,
+	}, nil
 }
 
 type storyContentS3Impl struct {
-	s3Client string
+	s3Client   s3.S3Client
+	contentMap map[string][]*StorySectionModel
 }
 
 func (s *storyContentS3Impl) GetContentByChapter(franchiseId int64, storyNum int, chapterNum int) ([]*StorySectionModel, error) {
