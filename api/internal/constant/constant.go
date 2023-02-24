5d3c32883158bc63cbc8b25d7c7465457e804327
Ze Huang <edisonz0718@gmail.com>
2023-02-24 15:27:20 -0800

    Update configs for staging

--- api/internal/constant/constant.go
+++ api/internal/constant/constant.go
@@ -1,7 +1,8 @@
 package constant
 
 const (
-	NftMaxLimit         = 500
-	S3ProjectNovaBucket = "project-nova-content"
+	NftMaxLimit = 500
+	//S3ProjectNovaBucket = "project-nova-content"
+	S3ProjectNovaBucket = "project-nova-content-staging"
 	S3ContentObject     = "content.json"
 )
