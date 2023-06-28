2f439a2cd75e2ad4d7524db7ca743890a42a8902
Ze Huang <ze.huang@storyprotocol.xyz>
2023-06-28 14:44:51 -0700

    v2 character APIs (#126)

---
+++ api/internal/entity/character.go
@@ -0,0 +1,133 @@
+package entity
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type CharacterMetadata struct {
+	Name      string `json:"name"`
+	Backstory string `json:"backstory"`
+}
+
+type CreateCharacterResp struct {
+	MediaUri string `json:"arweaveURI"`
+}
+
+type GetCharactersResp struct {
+	Name         string  `json:"name"`
+	OwnerAddress string  `json:"owner"`
+	CharacterId  *int64  `json:"tokenId"`
+	ImageUrl     *string `json:"img"`
+}
+
+type GetCharacterResp struct {
+	Name         string  `json:"name"`
+	OwnerAddress string  `json:"owner"`
+	CharacterId  *int64  `json:"tokenId"`
+	ImageUrl     *string `json:"img"`
+	Backstory    *string `json:"backstory"`
+	MediaUri     *string `json:"arweaveURI"`
+}
+
+type CreateCharacterRequestBody struct {
+	CharacterName *string `json:"name"`
+	ImageUrl      *string `json:"img"`
+	OwnerAddress  *string `json:"owner"`
+	Backstory     *string `json:"backstory"`
+}
+
+func (c *CreateCharacterRequestBody) Validate() error {
+	if c.CharacterName == nil {
+		return fmt.Errorf("character name is empty")
+	}
+	if c.ImageUrl == nil {
+		return fmt.Errorf("image url is empty")
+	}
+	if c.OwnerAddress == nil {
+		return fmt.Errorf("owner address is empty")
+	}
+	if c.Backstory == nil {
+		return fmt.Errorf("backstory is empty")
+	}
+	return nil
+}
+
+func (c *CreateCharacterRequestBody) ToCharacterInfoModel() *CharacterInfoModel {
+	characterInfo := &CharacterInfoModel{
+		ID: uuid.New().String(),
+	}
+	// Temporary: Add character id for FE testing
+	characterId := int64(rand.Uint32())
+	characterInfo.CharacterId = &characterId
+
+	if c.CharacterName != nil {
+		characterInfo.CharacterName = *c.CharacterName
+	}
+	if c.ImageUrl != nil {
+		characterInfo.ImageUrl = c.ImageUrl
+	}
+	if c.OwnerAddress != nil {
+		characterInfo.OwnerAddress = *c.OwnerAddress
+	}
+	if c.Backstory != nil {
+		characterInfo.Backstory = c.Backstory
+	}
+
+	return characterInfo
+}
+
+type CharacterInfoModel struct {
+	ID               string    `gorm:"primaryKey;column:id" json:"id"`
+	FranchiseAddress string    `json:"franchiseAddress"`
+	CharacterId      *int64    `json:"CharacterId"`
+	CharacterName    string    `json:"CharacterName"`
+	OwnerAddress     string    `json:"OwnerAddress"`
+	ImageUrl         *string   `json:"ImageUrl"`
+	Backstory        *string   `json:"backstory"`
+	MediaUri         *string   `json:"mediaUri"`
+	CreatedAt        time.Time `json:"createdAt"`
+	UpdatedAt        time.Time `json:"updatedAt"`
+}
+
+func (CharacterInfoModel) TableName() string {
+	return "character_info"
+}
+
+func (c *CharacterInfoModel) ToGetCharactersResp() (*GetCharactersResp, error) {
+	if c.CharacterId == nil {
+		return nil, fmt.Errorf("character id is nil")
+	}
+	resp := &GetCharactersResp{
+		Name:         c.CharacterName,
+		OwnerAddress: c.OwnerAddress,
+		CharacterId:  c.CharacterId,
+	}
+	if c.ImageUrl != nil {
+		resp.ImageUrl = c.ImageUrl
+	}
+	return resp, nil
+}
+
+func (c *CharacterInfoModel) ToGetCharacterResp() *GetCharacterResp {
+	resp := &GetCharacterResp{
+		Name:         c.CharacterName,
+		OwnerAddress: c.OwnerAddress,
+	}
+	if c.CharacterId != nil {
+		resp.CharacterId = c.CharacterId
+	}
+	if c.ImageUrl != nil {
+		resp.ImageUrl = c.ImageUrl
+	}
+	if c.Backstory != nil {
+		resp.Backstory = c.Backstory
+	}
+	if c.MediaUri != nil {
+		resp.MediaUri = c.MediaUri
+	}
+	return resp
+}
---
+++ api/internal/handler/protocol_handler_v2.go
@@ -0,0 +1,151 @@
+package handler
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/project-nova/backend/api/internal/entity"
+	"github.com/project-nova/backend/api/internal/repository"
+	"github.com/project-nova/backend/pkg/gateway"
+	"github.com/project-nova/backend/pkg/logger"
+	"github.com/project-nova/backend/pkg/utils"
+	"github.com/project-nova/backend/proto/v1/web3_gateway"
+)
+
+// GET /character/:franchiseAddress
+func NewGetCharactersHandlerV2(
+	characterInfoRepository repository.CharacterInfoRepository,
+) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		franchiseAddress, err := utils.SanitizeAddress(c.Param("franchiseAddress"))
+		if err != nil {
+			logger.Errorf("Invalid franchise address: %s", c.Param("franchiseAddress"))
+			c.JSON(http.StatusBadRequest, ErrorMessage("Invalid franchise address"))
+			return
+		}
+
+		characters, err := characterInfoRepository.GetCharacters(franchiseAddress)
+		if err != nil {
+			logger.Errorf("Failed to get characters: %v", err)
+			c.JSON(http.StatusInternalServerError, ErrorMessage("Internal server error"))
+			return
+		}
+
+		resp := []*entity.GetCharactersResp{}
+		for _, character := range characters {
+			charResp, err := character.ToGetCharactersResp()
+			if err != nil {
+				logger.Errorf("Failed to convert to character resp: %v", err)
+				continue
+			}
+			resp = append(resp, charResp)
+		}
+
+		c.JSON(http.StatusOK, resp)
+	}
+}
+
+// GET /character/:franchiseAddress/:tokenId
+func NewGetCharacterHandlerV2(
+	characterInfoRepository repository.CharacterInfoRepository,
+) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		franchiseAddress, err := utils.SanitizeAddress(c.Param("franchiseAddress"))
+		if err != nil {
+			logger.Errorf("Invalid franchise address: %s", c.Param("franchiseAddress"))
+			c.JSON(http.StatusBadRequest, ErrorMessage("Invalid franchise address"))
+			return
+		}
+
+		tokenId, err := strconv.Atoi(c.Param("tokenId"))
+		if err != nil {
+			logger.Errorf("Invalid token id: %s", c.Param("tokenId"))
+			c.JSON(http.StatusBadRequest, ErrorMessage("Invalid token id"))
+			return
+		}
+
+		character, err := characterInfoRepository.GetCharacter(franchiseAddress, int64(tokenId))
+		if err != nil {
+			logger.Errorf("Failed to get character, id %s: %v", c.Param("tokenId"), err)
+			c.JSON(http.StatusInternalServerError, ErrorMessage("Internal server error"))
+			return
+		}
+
+		characterResp := character.ToGetCharacterResp()
+		c.JSON(http.StatusOK, characterResp)
+	}
+}
+
+// POST /character/:franchiseAddress
+func NewCreateCharacterHandlerV2(
+	characterInfoRepository repository.CharacterInfoRepository,
+	web3Gateway gateway.Web3GatewayClient,
+) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		franchiseAddress, err := utils.SanitizeAddress(c.Param("franchiseAddress"))
+		if err != nil {
+			logger.Errorf("Invalid franchise address: %s", c.Param("franchiseAddress"))
+			c.JSON(http.StatusBadRequest, ErrorMessage("Invalid franchise address"))
+			return
+		}
+
+		var requestBody entity.CreateCharacterRequestBody
+		if err := c.BindJSON(&requestBody); err != nil {
+			logger.Errorf("Failed to read request body: %v", err)
+			c.JSON(http.StatusBadRequest, ErrorMessage("invalid request body"))
+			return
+		}
+		if err := requestBody.Validate(); err != nil {
+			logger.Errorf("Failed to validate request body: %v", err)
+			c.JSON(http.StatusBadRequest, ErrorMessage("invalid request body"))
+			return
+		}
+
+		characterInfo := requestBody.ToCharacterInfoModel()
+		characterInfo.FranchiseAddress = franchiseAddress
+
+		storyMeta := &entity.CharacterMetadata{
+			Name:      characterInfo.CharacterName,
+			Backstory: *characterInfo.Backstory,
+		}
+		storyMetaBytes, err := json.Marshal(storyMeta)
+		if err != nil {
+			logger.Errorf("Failed to marshal the story meta: %v", err)
+			c.JSON(http.StatusInternalServerError, ErrorMessage("Internal server error"))
+			return
+		}
+
+		contentBase64 := base64.StdEncoding.EncodeToString(storyMetaBytes)
+		resp, err := web3Gateway.UploadContent(&web3_gateway.UploadContentReq{
+			Storage:     web3_gateway.StorageType_ARWEAVE,
+			Content:     []byte(contentBase64),
+			ContentType: web3_gateway.ContentType_MARKDOWN,
+			Tags: []*web3_gateway.Tag{
+				{
+					Name:  "Content-Type",
+					Value: "application/json",
+				},
+			},
+		})
+		if err != nil {
+			logger.Errorf("Failed to upload content to web3-gateway: %v", err)
+			c.JSON(http.StatusInternalServerError, ErrorMessage("Internal server error"))
+			return
+		}
+
+		characterInfo.MediaUri = &resp.ContentUrl
+		err = characterInfoRepository.CreateCharacter(characterInfo)
+		if err != nil {
+			logger.Errorf("Failed to create character info in db: %v", err)
+			c.JSON(http.StatusInternalServerError, ErrorMessage("Internal server error"))
+			return
+		}
+
+		c.JSON(http.StatusOK, &entity.CreateCharacterResp{
+			MediaUri: resp.ContentUrl,
+		})
+	}
+}
