9ba251d1c04be17758ff3ea706ea20afb17bab07
Rex Ren <rex@dimefi.com>
2023-11-05 21:59:39 -0800

    remove unnecessary code

--- api/internal/handler/protocol_handler_v2.go
+++ api/internal/handler/protocol_handler_v2.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
@@ -229,20 +228,8 @@ func NewUploadFileHandlerV2(
 	web3Gateway gateway.Web3GatewayClient,
 ) func(c *gin.Context) {
 	return func(c *gin.Context) {
-		body, err := ioutil.ReadAll(c.Request.Body)
-		if err != nil {
-			// Handle error
-			logger.Errorf("Failed to read request body: %v", err)
-			c.JSON(http.StatusBadRequest, ErrorMessage("Invalid request"))
-			return
-		}
-
-		// Log the request body
-		logger.Infof("Request body: %s", string(body))
-
-		// Reset the request body so it can be read again later
-		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-
+		headers := c.Request.Header
+		logger.Infof("headers: %v", headers)
 		form, err := c.MultipartForm()
 		if err != nil {
 			logger.Errorf("Failed to read multipart form: %v", err)
